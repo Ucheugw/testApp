30867a47ce796fc5f49644c3556e52b12b727a6c
Uche Ugwoaba <ucheugw@gmail.com>
2017-03-12 19:20:52 -0400

    UU first commit, before HTTP section of hero tutorial

---
+++ firstAngular2/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: []
+})
+export class AppComponent {
+    title = 'Uche Ugwoaba, tryna get stuff done';
+
+}
+
+
+
+
+
+
+
---
+++ firstAngular2/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './shared/hero.service';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }, {
+        path: 'dashboard',
+        component: DashboardComponent
+      }, {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
